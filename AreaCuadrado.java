ef90cce8b751dd3165f0797afc7533fb958328f7
AMorGar <117309159+AMorGar@users.noreply.github.com>
2022-11-03 16:04:37 +0100

    Create AreaCuadrado.java

---
+++ AreaCuadrado.java
@@ -0,0 +1,17 @@
+
+/*Este programa se encarga de carcular el area de un cuadrado.
+ Alejandro Moreno Garrido*/ 
+
+public class AreaCuadrado {
+	
+	public static void main (String[] args) {
+//inicializamos la variable a que es la que se va a aencargar de realizar los calculos//
+		int a;
+//Realizamos los calculos//
+	a=5*5;
+//Imprimimos en pantalla//
+		System.out.println("El resultado del area de un cuadrado de lado 5 es= "+a);
+		
+
+}}
+
