ec1c76b4593998b16b6960ca52afda5df932f931
AMorGar <117309159+AMorGar@users.noreply.github.com>
2022-11-10 18:06:04 +0100

    Update Euros.java

--- Ejercicios7/Euros.java
+++ Ejercicios7/Euros.java
@@ -7,14 +7,16 @@
 public class Euros {
 	
 	public static void main (String[] args) {
-		
+//Declaramos las variables		
 		int equivalencia,pesetas,solucion;
-		
+//Asignamos valores		
 		pesetas = 1000;
 		equivalencia=166;
+//Realizamos las operaciones y asignamos el valor a la variable
 		solucion= pesetas/equivalencia;
 		
-				System.out.println(pesetas+" euros son:"+ solucion+" pesetas");
+//Imprimimos por pantalla				
+		System.out.println(pesetas+" euros son:"+ solucion+" pesetas");
 		
 	}
 	
