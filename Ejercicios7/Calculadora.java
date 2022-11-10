89c553f0eb64afbfc4cf4bdbc3c3e3c100e6358e
AMorGar <117309159+AMorGar@users.noreply.github.com>
2022-11-10 18:04:30 +0100

    Update Calculadora.java

--- Ejercicios7/Calculadora.java
+++ Ejercicios7/Calculadora.java
@@ -7,14 +7,16 @@
 public class Calculadora {
 	
 	public static void main (String[] args) {
+//Declaramos variables y asignamos valores a ellas
 		int x=144;
 		int y=999;
 		double suma,resta,division,multiplicacion;
-		
+//Realizamos las operaciones y guardamos en las variables indicadas 		
 		suma=x+y;
 		resta=y-x;
 		division=y/x;
 		multiplicacion=x*y;
+//Imprimimos en pantalla
 				System.out.println("La suma es= "+ suma);
 				System.out.println("La resta es= "+ resta);
 				System.out.println("La division es= "+ division);
