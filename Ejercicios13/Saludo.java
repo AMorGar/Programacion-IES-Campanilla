6a983891aaa41c598e8e576a1d9aa989f1ed0e26
AMorGar <117309159+AMorGar@users.noreply.github.com>
2022-11-17 19:21:07 +0100

    Update Saludo.java

--- Ejercicios13/Saludo.java
+++ Ejercicios13/Saludo.java
@@ -15,9 +15,9 @@ public class Saludo {
 	int hora = s.nextInt();
 
 
-	if ((hora>=6) || (hora<=12)) {
+	if ((hora>=6) && (hora<=12)) {
 	  System.out.print("Buenos dias");
-	} else if((hora>=13) || (hora<=20)) {
+	} else if((hora>=13) && (hora<=20)) {
 	  System.out.println("Buenas tardes");
 	} else  {
 	  System.out.println("Buenas noches");
