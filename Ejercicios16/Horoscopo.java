9548a73e7c5112052cae857dcd99d49380bd4bba
AMorGar <117309159+AMorGar@users.noreply.github.com>
2022-11-22 17:00:33 +0100

    Update Horoscopo.java

--- Ejercicios16/Horoscopo.java
+++ Ejercicios16/Horoscopo.java
@@ -122,3 +122,93 @@ public class Horoscopo {
 	
 }
 }
+/**
+switch(mes) {
+      case 1:
+        if (dia < 21) {
+          System.out.print("capricornio");
+        } else {
+         System.out.print("acuario");
+        }
+        break;
+      case 2:
+        if (dia < 20) {
+          System.out.print("acuario");
+        } else {
+          System.out.print("piscis");
+        }
+        break;
+      case 3:
+        if (dia < 21) {
+          System.out.print( "piscis");
+        } else {
+        System.out.print("aries");
+        }
+        break;
+      case 4:
+        if (dia < 21) {
+         System.out.print( "aries");
+        } else {
+         System.out.print("tauro");
+        }
+        break;
+      case 5:
+        if (dia < 20) {
+          System.out.print("tauro");
+        } else {
+         System.out.print("géminis");
+        }
+        break;
+      case 6:
+        if (dia < 22) {
+          System.out.print("géminis");
+        } else {
+          System.out.print("cáncer");
+        }
+        break;
+      case 7:
+        if (dia < 22) {
+          System.out.print("cáncer");
+        } else {
+         System.out.print("Leo");
+        }
+        break;
+      case 8:
+        if (dia < 24) {
+          System.out.print(leo");
+        } else {
+          System.out.print("virgo");
+        }
+        break;
+      case 9:
+        if (dia < 23) {
+          System.out.print( "virgo");
+        } else {
+          System.out.print( "libra");
+        }
+        break;
+      case 10:
+        if (dia < 23) {
+          System.out.print( "libra");
+        } else {
+          System.out.print( "escorpio");
+        }
+        break;
+      case 11:
+        if (dia < 23) {
+          System.out.print("escorpio");
+        } else {
+          System.out.print("sagitario");
+        }
+        break;
+      case 12:
+        if (dia < 21) {
+          System.out.print("sagitario");
+        } else {
+          System.out.print("capricornio");
+        }
+        break;
+      default:
+    }
+**/
+
