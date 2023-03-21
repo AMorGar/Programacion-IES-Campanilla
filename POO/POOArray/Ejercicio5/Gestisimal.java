bbae90fc8c9ae28c84bcaf83313e3447a11e9d7b
AMorGar <117309159+AMorGar@users.noreply.github.com>
2023-03-21 21:36:54 +0100

    Update Gestisimal.java

--- POO/POOArray/Ejercicio5/Gestisimal.java
+++ POO/POOArray/Ejercicio5/Gestisimal.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Gestisimal {
 
-  // N determina el tamaño del array
+// N determina el tamaño del array
   static final int N = 100;
 //se declara el array con el tamaño de la variable antes creada
   static Articulo[] articulos = new Articulo[N];
 
   public static void main(String[] args) {
-
-    // Inicializa el array
-    for(int i = 0; i < N; i++) {
-      Gestisimal.articulos[i] = new Articulo();
+// Inicializa el array
+    for(int contador = 0; contador < N; contador++) {
+      Gestisimal.articulos[contador] = new Articulo();
     }
 //Se declaran todas las variables
     int opcion;
-    int stockIntroducido;
+    int cantidad;
     double precioDeCompraIntroducido;
     double precioDeVentaIntroducido;
     String codigoIntroducido = "";
@@ -52,7 +51,7 @@ public class Gestisimal {
         case 2:
         System.out.print("NUEVO ARTÍCULO");
           
-          if (primeraPosicionLibre() == N) {
+          if (Buscarhueco() == N) {
             System.out.println("Lo siento, la base de datos está llena.");
           } else {
 //Introduce datos de cada articulo
@@ -61,11 +60,11 @@ public class Gestisimal {
 // Comprueba que el código introducido no se repita
             do {
               codigoIntroducido = s.nextLine();
-              if (existeCodigo(codigoIntroducido)) {
+              if (existe(codigoIntroducido)) {
                 System.out.println("Ese código ya existe en la base de datos.");
                 System.out.print("Introduzca otro código: ");
               }
-            } while (existeCodigo(codigoIntroducido));
+            } while (existe(codigoIntroducido));
             
             System.out.print("Descripcion: ");
             descripcionIntroducida = s.nextLine();
@@ -74,12 +73,12 @@ public class Gestisimal {
             System.out.print("Precio de venta: ");
             precioDeVentaIntroducido = Double.parseDouble(s.nextLine());
             System.out.print("Stock: ");
-            stockIntroducido = Integer.parseInt(s.nextLine());
+            cantidad = Integer.parseInt(s.nextLine());
 
             // Crea el nuevo artículo
-            articulos[primeraPosicionLibre()] = new Articulo(
+            articulos[Buscarhueco()] = new Articulo(
               codigoIntroducido, descripcionIntroducida, precioDeCompraIntroducido,
-              precioDeVentaIntroducido, stockIntroducido);
+              precioDeVentaIntroducido, cantidad);
           }         
           break;
         case 3:
@@ -88,10 +87,10 @@ public class Gestisimal {
           System.out.print("Por favor, introduzca el código del artículo: ");
           codigoIntroducido = s.nextLine();
     
-          if (!existeCodigo(codigoIntroducido)) {
+          if (!existe(codigoIntroducido)) {
             System.out.println("Lo siento, el código introducido no existe.");
           } else {
-            articulos[posicionConCodigo(codigoIntroducido)].setCodigo("LIBRE");
+            articulos[localizar(codigoIntroducido)].setCodigo("LIBRE");
             System.out.println("Artículo borrado.");  
           }
           
@@ -102,13 +101,13 @@ public class Gestisimal {
           System.out.print("Por favor, introduzca el código del artículo: ");
           codigoIntroducido = s.nextLine();
 
-          if (!existeCodigo(codigoIntroducido)) {
+          if (!existe(codigoIntroducido)) {
             System.out.println("Lo siento, el código introducido no existe.");
           } else {
             System.out.print("Introduzca los nuevos datos del artículo");
             System.out.println(" o INTRO para dejarlos igual.");
       
-            int i = posicionConCodigo(codigoIntroducido);
+            int i = localizar(codigoIntroducido);
 
             System.out.println("Código: " + articulos[i].getCodigo());
             System.out.print("Nuevo código: ");
@@ -124,11 +123,11 @@ public class Gestisimal {
               articulos[i].setDescripcion(descripcionIntroducida);
             }
             
-            System.out.println("Precio de compra: " + articulos[i].getPrecioDeCompra());
+            System.out.println("Precio de compra: " + articulos[i].getPrecioTotal());
             System.out.print("Nuevo precio de compra: ");
             String precioDeCompraIntroducidoString = s.nextLine();
             if (!precioDeCompraIntroducidoString.equals("")) {
-              articulos[i].setPrecioDeCompra(Double.parseDouble(precioDeCompraIntroducidoString));
+              articulos[i].setPrecioTotal(Double.parseDouble(precioDeCompraIntroducidoString));
             }
             System.out.println("Precio de venta: " + articulos[i].getPrecioDeVenta());
             System.out.print("Nuevo precio de venta: ");
@@ -136,11 +135,11 @@ public class Gestisimal {
             if (!precioDeVentaIntroducidoString.equals("")) {
               articulos[i].setPrecioDeVenta(Double.parseDouble(precioDeVentaIntroducidoString));
             }
-            System.out.println("Stock: " + articulos[i].getStock());
+            System.out.println("Stock: " + articulos[i].getalmacen());
             System.out.print("Nuevo stock: ");
             String stockIntroducidoString = s.nextLine();
             if (!stockIntroducidoString.equals("")) {
-              articulos[i].setStock(Integer.parseInt(stockIntroducidoString));
+              articulos[i].setalmacen(Integer.parseInt(stockIntroducidoString));
             }
           }
           
@@ -151,16 +150,16 @@ public class Gestisimal {
             System.out.print("Por favor, introduzca el código del artículo: ");
             codigoIntroducido = s.nextLine();
         
-            if (!existeCodigo(codigoIntroducido)) {
+            if (!existe(codigoIntroducido)) {
               System.out.println("Lo siento, el código introducido no existe.");
             } else {
-              int i = posicionConCodigo(codigoIntroducido);
+              int i = localizar(codigoIntroducido);
               System.out.println("Entrada de mercancía del siguiente artículo: ");
               System.out.println(articulos[i]);
               System.out.print("Introduzca el número de unidades que entran: ");
-              String stockIntroducidoString = s.nextLine();
-              articulos[i].setStock(
-                Integer.parseInt(stockIntroducidoString) + articulos[i].getStock());
+              String cantidadString = s.nextLine();
+              articulos[i].setalmacen(
+                Integer.parseInt(cantidadString) + articulos[i].getalmacen());
               System.out.println("La mercancía ha entrado en el almacén.");  
             }
             
@@ -171,16 +170,16 @@ public class Gestisimal {
               System.out.print("Por favor, introduzca el código del artículo: ");
               codigoIntroducido = s.nextLine();
 
-              if (!existeCodigo(codigoIntroducido)) {
+              if (!existe(codigoIntroducido)) {
                 System.out.println("Lo siento, el código introducido no existe.");
               } else {
-                int i = posicionConCodigo(codigoIntroducido);
+                int i = localizar(codigoIntroducido);
                 System.out.println("Salida de mercancía del siguiente artículo: ");
                 System.out.println(articulos[i]);
                 System.out.print("Introduzca el número de unidades que desea sacar del almacén: ");
-                stockIntroducido = Integer.parseInt(s.nextLine());
-                if (articulos[i].getStock() - stockIntroducido > 0) {
-                  articulos[i].setStock(articulos[i].getStock() - stockIntroducido);
+                cantidad = Integer.parseInt(s.nextLine());
+                if (articulos[i].getalmacen() - cantidad > 0) {
+                  articulos[i].setalmacen(articulos[i].getalmacen() - cantidad);
                   System.out.println("La mercancía ha salido del almacén.");
                 } else {
                   System.out.println("Lo siento, no se pueden sacar tantas unidades.");
@@ -192,7 +191,7 @@ public class Gestisimal {
     } while (opcion != 7);
   }
 //Busca un hueco libre
-  public static int primeraPosicionLibre() {
+  public static int Buscarhueco() {
     for (int i = 0; i < articulos.length; i++) {
       if (articulos[i].getCodigo().equals("LIBRE")) {
         return i;
@@ -201,7 +200,7 @@ public class Gestisimal {
     return -1;
   }
 //Comprueba si existe el codigo o no
-  public static boolean existeCodigo(String codigo) {
+  public static boolean existe(String codigo) {
     for (Articulo a : articulos) {
       if (a.getCodigo().equals(codigo)) {
         return true;
@@ -212,7 +211,7 @@ public class Gestisimal {
 
 
 //Devuelve la posición en la que se encuentra x articulo
-  public static int posicionConCodigo(String codigo) {
+  public static int localizar(String codigo) {
     for (int i = 0; i < articulos.length; i++) {
       if (articulos[i].getCodigo().equals(codigo)) {
         return i;
